3272fea081142780fb03e07fecf8d95556786e34
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-01-26 13:07:46 +0530

    Started HTTP section

---
+++ 07-http/01-simple-server/simple-client.js
@@ -0,0 +1,66 @@
+const http = require("node:http");
+
+// * An Agent is responsible for managing connection persistence and reuse for HTTP clients
+const agent = http.Agent({ keepAlive: true });
+
+// * Request Options
+
+const options = {
+  agent: agent,
+  hostname: "localhost",
+  port: 8050,
+  method: "POST",
+  path: "/create-post",
+  headers: {
+    "Content-Type": "application/json",
+    "name": "Swapnil"
+  },
+};
+
+// * It represents an in-progress request whose header has already been queued.
+const request = http.request(options);
+
+// * This is emitted only once
+request.on("response", (res) => {
+  // console.log("Response ", res);
+
+  console.log("------------ HEADERS: ------------");
+  console.log(res.headers);
+
+  console.log("------------ STATUS: ------------");
+  console.log(res.statusCode);
+
+  console.log("------------ BODY: ------------");
+  res.on('data', (chunk) => {
+    console.log("Response Data ", chunk.toString('utf-8'));
+  })
+});
+
+request.write(JSON.stringify({ title: "Post 1!" }));
+request.end(JSON.stringify({ body: "Post 1 Body!" }));
+
+const request2 = http.request(options);
+request2.write(JSON.stringify({ title: "Post 2!" }));
+request2.end(JSON.stringify({ body: "Post 2 Body!" }));
+
+// * This is emitted only once
+request2.on("response", (res) => {
+  // console.log("Response ", res);
+
+  console.log("------------ HEADERS: ------------");
+  console.log(res.headers);
+
+  console.log("------------ STATUS: ------------");
+  console.log(res.statusCode);
+
+  console.log("------------ BODY: ------------");
+  res.on('data', (chunk) => {
+    console.log("Response Data ", chunk.toString('utf-8'));
+  })
+});
+
+/*
+ * Note 
+ * You can either use Content-Length or Transfer Encoding
+ * You cannot use both at same time
+ */
---
+++ 07-http/01-simple-server/simple-server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("------------ METHOD: ------------");
+  console.log(req.method);
+
+  console.log("------------ URL: ------------");
+  console.log(req.url);
+
+  console.log("------------ HEADERS: ------------");
+  console.log(req.headers);
+  const name = req.headers.name;
+
+  let data = {}
+  req.on('data', (chunk) => {
+    // console.log("Data: ", chunk.toString());
+    Object.assign(data, JSON.parse(chunk.toString()))
+  })
+
+  req.on('end', () => {
+    console.log('Name: ', name);
+    console.log('Data: ', data);
+
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    })
+    res.write(JSON.stringify({ message: `Hello ${name} your Post Created!` }));
+  })  
+
+
+})
+
+server.listen(8050, () => {
+  console.log("Server listening on 8050");
+})
